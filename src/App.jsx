e672ac0b12e8c37a62fa6953978e7ca8213e5cc9
Amit <amit@ThinkPad-T430-bade6184.localdomain>
2024-08-31 17:26:22 +0530

    fatal: pathspec '.a' did not match any files bash: line 1: f: command not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Home from './compnents/Home'
+import CreateUser from './compnents/CreateUser'
+import ShowUser from './compnents/ShowUser'
 const App = () => {
   return (
     <Routes>
-      
       <Route path='/' element={<Home />} />
+      <Route path='/CreateUser' element={<CreateUser></CreateUser>} />
+      <Route path='/ShowUser' element={<ShowUser></ShowUser>} />
     </Routes>
   )
 }
--- src/compnents/Home.jsx
+++ src/compnents/Home.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import Getuser from './Getuser'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/CreateUser');
+  };
+  const show= () => {
+    navigate('/ShowUser');
+  };
   return (
-    <div>
-      <Getuser></Getuser>
+    <div className='flex justify-center  mt-[10%]'>
+      <button onClick={handleClick}
+      className='rounded-md bg-blue-500 '
+      >Create user</button>
+
+<button onClick={show}
+      className='rounded-md bg-blue-500 '
+      >Show User</button>
     </div>
   )
 }
-
 export default Home
